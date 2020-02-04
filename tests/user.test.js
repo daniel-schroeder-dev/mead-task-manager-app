fb6bd6c61a15cbe26cd452c51fd957231b4d87d3
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-02-04 13:09:26 -0600

    Refactor user.test.js to use res variable after running request and group tests in 'describe' blocks.

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,63 +1,75 @@
 const request = require('supertest');
 const app = require('../app');
 const User = require('../src/models/user');
+const mongoose = require('mongoose');
 
-const testUser = {
-  name: 'test',
-  email: 'testUser@gmail.com',
+const existingUser = {
+  name: 'existing',
+  email: 'existingUser@gmail.com',
   password: 'myPass123',
 };
 
 beforeAll(async () => {
   await User.deleteMany();
-  await new User(testUser).save();
+  await new User(existingUser).save();
 });
 
-test('Should signup a new user', async () => {
-  await request(app)
-    .post('/users')
-    .send({
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+describe('User signup routes', () => {
+  
+  test('Should signup a new user', async () => {
+    
+    const newUser = {
       name: 'Daniel',
       email: 'daniel@gmail.com',
       password: 'myPass123',
-    })
-    .expect(201)
-    .expect((res) => {
-      expect(res.body.email).toBe('daniel@gmail.com');
-    });
+    };
+    
+    const res = await request(app).post('/users').send(newUser);
+    
+    expect(res.statusCode).toBe(201);
+    expect(res.body.email).toBe(newUser.email);
+  
+  });
+
+  test('Should fail to signup a new user', async () => {
+    
+    const res = await request(app).post('/users').send({ invalid: 'Data' });
+    
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toMatch(/^User validation failed/);
+  
+  });
+
 });
 
-test('Should fail to signup a new user', async () => {
-  await request(app)
-    .post('/users')
-    .send({
-      invalid: 'Data',
-    })
-    .expect(400)
-    .expect((res) => {
-      expect(res.body).toMatch(/^User validation failed/);
-    });
-});
+describe('User login routes', () => {
+  
+  test('Should login existing user', async () => {
+    
+    const res = await request(app).post('/users/login').send(existingUser);
+    
+    expect(res.statusCode).toBe(200);
+    expect(res.body.email).toBe(existingUser.email);
+  
+  });
 
-test('Should login existing user', async () => {
-  await request(app)
-    .post('/users/login')
-    .send(testUser)
-    .expect(200)
-    .expect((res) => {
-      expect(res.body.email).toBe(testUser.email);
-    });
-});
-
-test('Should not login nonexistent user', async () => {
-  await request(app)
-    .post('/users/login')
-    .send({
+  test('Should not login nonexistent user', async () => {
+    
+    const invalidUser = {
       email: 'nope@gmail.com',
       password: 'nonsuchpass',
-    })
-    .expect(400)
-    .expect((res) => {
-      expect(res.body).toBe('Login failed');
-    });
+    };
+
+    const res = await request(app).post('/users/login').send()
+    
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toBe('Login failed');
+  
+  });
+  
 });
+
